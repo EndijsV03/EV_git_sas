f7c901252f4056a9869ef5cf8f48f20a427cfab8
Endijs Vanags <ev2830@gymn.local>
2021-11-16 09:20:53 +0200

    https://github.com/EndijsV03/EV_git_sas.git

--- main.cpp
+++ main.cpp
@@ -2,23 +2,25 @@
 
 using namespace std;
 
-int SAS(int x, int y);
+int SAS(int x, int y, int z);
 int main()
 {
-    int p, o, s;
+    int p, o, s, t;
     cout << "Ievadiet pirmo skaitli: ";
     cin >> p;
     cout << "Ievadiet otro skaitli: ";
     cin >> o;
+    cout << "Ievadiet treso skaitli: ";
+    cin >> t;
 
-    cout << "Jusu skaitlu summa ir "<< SAS(p,o) << endl;
+    cout << "Jusu skaitlu summa ir "<< SAS(p,o,t) << endl;
     return 0;
 }
 
-int SAS(int x, int y)
+int SAS(int x, int y, int z)
 {
     int s;
-    s = x + y;
+    s = x + y + z;
 
     return s;
 }
